bf51ae670bedcd30fb296b55606e619c67c9407f
Lakshya SIngh <iD024>
2025-09-13 04:36:22 +0530

    fixed import bug

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo1 from "../assets/logo1.png";
-import logo2 from "../assets/logo2.png";
 import "./css/LandingPage.css";
 
 function LandingPage() {
